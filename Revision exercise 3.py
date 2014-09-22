c165b64aad85a328ea904587c00f8877d36fad00
Mateusz Sacha <45197@longroad.ac.uk>
2014-09-22 10:25:49 +0100

    need to finish revision exercise 5

--- Revision exercise 3.py
+++ Revision exercise 3.py
@@ -1,9 +1,9 @@
 #Mateusz Sacha
 #17/09/2014
-#assignment class exercises 4
+#assignment class exercises 3
 number = int(input("enter a number"))
 number2 = int(input("enter a different number"))
 answer = number // number2
-print ("{0}".format(answer))
-
+answer1 = number % number2
+print ("{0}remainder{1}".format(answer,answer1))
 
---
+++ revision exercise 5.py
@@ -0,0 +1,15 @@
+# Mateusz Sacha
+# 22/09/2014
+# revision exercise 5
+number1 = 120 #width
+number2 = 150 #length
+number3 = 200 #height
+
+number4 = 80 #width
+number5 = 50 #length
+number6 = 190 #height
+
+answer = (number1 * number2 * number3)
+answer1 = (number4 * number5 * number6)
+print(answer - answer1)
+
